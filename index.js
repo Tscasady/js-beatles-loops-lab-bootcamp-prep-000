dffae5a950b1846029885af9cfa7d3017b2c8d0c
Thomas Casady <tscasady@gmail.com>
2018-02-16 00:45:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicArray, instrumentArray){
   var newArray = []
   for(var i = 0; i < musicArray.length; i++){
-    var newString = musicArray[i] + 'plays' + instrumentArray[i]
+    var newString = musicArray[i] + ' plays ' + instrumentArray[i]
     newArray.push(newString)
   }
   return newArray
